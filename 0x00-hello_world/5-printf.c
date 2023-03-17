4cb791934a4a4a1e1374d6ef8a0e3b5fa427385d
soufiane benhabboul <soufiane0bn@gmail.com>
2023-03-16 17:54:06 -0700

    tasks C done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Enrty point
+ *
+ * Description: A C program that print with printf function.
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
