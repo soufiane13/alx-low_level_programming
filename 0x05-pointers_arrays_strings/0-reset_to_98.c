220521fe5b50edc227d0ee9e0799f5c972ed883f
soufiane benhabboul <soufiane0bn@gmail.com>
2023-03-28 19:10:41 -0700

    pointer task 1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_90 - takes a pointer to an int parameter
+ * 			and updates the value it pionts to 98
+ *
+ * 0n: int parameter
+ *
+ * Return: Nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
