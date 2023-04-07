b5d2815fbdd7fd050893a4d4fe420a662257cebf
soufiane benhabboul <soufiane0bn@gmail.com>
2023-04-06 18:45:56 -0700

    function gives the lenght of string betty error fixed

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,17 +1,17 @@
 #include "main.h"
 #include <stdio.h>
 
+
 /**
- * _strlen_recursion - function that return the lenght of a string
- * @s: pointer to a string
- * @l: integer get the lenght of string
- *
- * Return: a integer , the lenght th string 
+ * _strlen_recursion - function gives the lenght
+ * @s:pointer to string
+ * Return: int
  */
 
 int _strlen_recursion(char *s)
 {
 	int l = 0;
+
 	if (*s > '\0')
 	{
 		l = l + _strlen_recursion(s + 1) + 1;
