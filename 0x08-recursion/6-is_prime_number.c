e27d508f5da19b0d485704eb63c39a29e2d03019
soufiane benhabboul <soufiane0bn@gmail.com>
2023-04-16 13:50:14 -0700

    is it prime number task

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,29 +2,38 @@
 #include <stdio.h>
 
 /**
- * main - check the code
+ * is_prime_number - return if an number is prime
+ * @n: the checked number
+ * @i: interation
  *
- * Return: Always 0.
+ * Return: 0 if it not prime 1 if it prime.
  */
-int main(void)
-{
-    int r;
+int _prime(int n, int i);
 
-    r = is_prime_number(1);
-    printf("%d\n", r);
-    r = is_prime_number(1024);
-    printf("%d\n", r);
-    r = is_prime_number(16);
-    printf("%d\n", r);
-    r = is_prime_number(17);
-    printf("%d\n", r);
-    r = is_prime_number(25);
-    printf("%d\n", r);
-    r = is_prime_number(-1);
-    printf("%d\n", r);
-    r = is_prime_number(113);
-    printf("%d\n", r);
-    r = is_prime_number(7919);
-    printf("%d\n", r);
-    return (0);
+int is_prime_number(int n)
+{
+	return (_prime(n, 1));
+}
+
+/**
+ * _prime - check if the number is prime
+ * @n: the cheked number
+ * @i: interation.
+ *
+ * Return: 1 for prime number or 0 if it not
+ */
+
+int _prime(int n, int i)
+{
+	if (n <= 1)
+		return (0);
+
+	if (n % i == 0 && i > 1)
+		return (0);
+
+	if ((n / i) < i)
+		return (1);
+
+	return (_prime(n, i + 1));
+
 }
