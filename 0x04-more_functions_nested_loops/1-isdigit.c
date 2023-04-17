4abf9d2a057a9972d1f7b657d8eb39e7abfbf8b9
soufiane benhabboul <soufiane0bn@gmail.com>
2023-04-17 09:59:23 -0700

    task 1 struct dog

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int _isdigit(int c)
+{
+	return (0);
+}
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a new struct dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: a new type of dog 
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
