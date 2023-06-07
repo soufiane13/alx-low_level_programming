85bbc8468f1c4b7808abf0fbff76307eae385177
soufiane benhabboul <soufiane0bn@gmail.com>
2023-06-07 14:37:36 -0700

    fixing betty style problem

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,13 +3,13 @@
 /**
  * _strlen_recursion - function that mesure the lenght of a string
  * @s: pointer to a string
- * @l: integer
  * Return:void
  */
 
 int _strlen_recursion(char *s)
 {
 	int l = 0;
+
 	if (*s > '\0')
 	{
 		l += 1;
