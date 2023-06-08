78e5d78b97c931b36381922aa2df396d923e6851
soufiane benhabboul <soufiane0bn@gmail.com>
2023-06-07 17:03:55 -0700

    task 6 ready to check

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <srdio.h>
+/**
+ * is_prime_number - function prime number
+ * @n: integer
+ * @o: int
+ * Return: 0 or 1
+ */
+
+int check(int n, int o);
+int is_prime_number(int n)
+{
+	return (check(n, 2));
+}
+/**
+ * check - function chekes if number is prime
+ * @n: int
+ * @o: int
+ * Return: int
+ */
+int check(int n, int o)
+{
+	if (o >= n && n > 1)
+		return (1);
+	else if (n % o == 0 || n <= 1)
+		return (0);
+	else
+		return (check(n, o + 1));
+}
