b3bfe5909850c06d142ee9712dc1b5a47edd622a
soufiane benhabboul <soufiane0bn@gmail.com>
2023-06-09 18:45:29 -0700

    task 1 ready to check

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,7 +7,7 @@
  * Return: 0 or 1
  */
 
-int main(int argc, char *argv[] __attribute__(()unused))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 	return (0);
