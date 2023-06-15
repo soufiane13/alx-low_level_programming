7f78e2841d0e9e3e3e19565b5e2696bd9ee7ec92
soufiane benhabboul <soufiane0bn@gmail.com>
2023-06-15 12:32:46 -0700

    task 0 ready to check

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * *malloc_checked - allocates memory with exit error if failed
+ * @b: int
+ * Return: null or pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *n = malloc(b);
+
+	if (n == 0)
+		exit(98);
+
+	return (n);
+}
