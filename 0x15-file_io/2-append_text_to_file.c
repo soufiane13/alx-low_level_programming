e9f65670e0b2790945feced86d9ba8cb9a061a94
soufiane benhabboul <soufiane0bn@gmail.com>
2024-07-18 08:46:46 -0700

    fixing misspelling of variabal lenght

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -23,7 +23,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 
 	o = open(filename, O_WRONLY | O_APPEND);
-	w = write(o, text_content, len);
+	w = write(o, text_content, length);
 
 	if (o == -1 || w == -1)
 		return (-1);
