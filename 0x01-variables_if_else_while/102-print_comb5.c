70ca226b220aeabb661d2d55cbccb77e947935d4
soufiane benhabboul <soufiane0bn@gmail.com>
2024-07-29 05:56:42 -0700

    task ready to review and fixing some betty style errors

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of two two-digit numbers,
+ *         from 0-99, separated by a comma followed by a space.
+ *
+ * Return: 0.
+ */
+int main(void)
+{
+	int n1, n2;
+
+	for (n1 = 0; n1 <= 98; n1++)
+	{
+		for (n2 = n1 + 1; n2 <= 99; n2++)
+		{
+			putchar((n1 / 10) + '0');
+			putchar((n1 % 10) + '0');
+			putchar(' ');
+			putchar((n2 / 10) + '0');
+			putchar((n2 % 10) + '0');
+
+			if (n1 == 98 && n2 == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
